e12b7327abe335ca7c7d2961df41949a751ae111
Danilo Couto de Souza <danilo.oceano@gmail.com>
2023-09-17 12:28:32 -0300

    Adjusting cbar

--- src_figures/precipitation.py
+++ src_figures/precipitation.py
@@ -6,7 +6,7 @@
 #    By: Danilo  <danilo.oceano@gmail.com>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/08 09:52:10 by Danilo            #+#    #+#              #
-#    Updated: 2023/09/17 12:26:16 by Danilo           ###   ########.fr        #
+#    Updated: 2023/09/17 12:28:26 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -298,12 +298,13 @@ def plot_precipitation_panels(data, experiments, figures_directory, grid, zoom=F
             ax.coastlines(zorder=1)            
             i += 1
 
-    cb_axes = fig.add_axes([0.85, 0.18, 0.04, 0.6])
-    fig.colorbar(cf, cax=cb_axes, orientation="vertical")
-
     if zoom:
+        cb_axes = fig.add_axes([0.85, 0.18, 0.04, 0.6])
+        fig.colorbar(cf, cax=cb_axes, orientation="vertical")
         fig.subplots_adjust(wspace=0.1, hspace=0, right=0.8)
     else:
+        cb_axes = fig.add_axes([1.05, 0.18, 0.04, 0.6])
+        fig.colorbar(cf, cax=cb_axes, orientation="vertical")
         plt.tight_layout()
 
     os.makedirs(figures_directory, exist_ok=True)
