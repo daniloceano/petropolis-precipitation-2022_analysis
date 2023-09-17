7efbe67c0bb8b223e6220fd482d8138ccc248b67
Danilo Couto de Souza <danilo.oceano@gmail.com>
2023-09-17 12:01:46 -0300

    Updated zoom region

--- src_figures/precipitation.py
+++ src_figures/precipitation.py
@@ -6,7 +6,7 @@
 #    By: Danilo  <danilo.oceano@gmail.com>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/02/08 09:52:10 by Danilo            #+#    #+#              #
-#    Updated: 2023/09/17 11:17:50 by Danilo           ###   ########.fr        #
+#    Updated: 2023/09/17 12:01:36 by Danilo           ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -230,9 +230,13 @@ def plot_precipitation_panels(data, experiments, figures_directory, grid, zoom=F
             'lon': [-45, -40],
             'lat': [-23.30, -20.30]
         },
-        'zoom': {
+        'polygon': {
             'lon': [-43.2, -43],
             'lat': [-22.4, -22.2]
+        },
+        'zoom': {
+            'lon': [-43.7, -42.5],
+            'lat': [-22.9, -21.8]
         }
     }
 
@@ -269,7 +273,7 @@ def plot_precipitation_panels(data, experiments, figures_directory, grid, zoom=F
                 ax.set_extent(domain_coords['full']['lon'] + domain_coords['full']['lat'], crs=datacrs)
                 ax.text(-45, -19.8, f'{experiment}: {max_prec:.2f}', fontdict={'size': 14})
                 # Plot polygon around smaller domain
-                zoom_coords = domain_coords['zoom']
+                zoom_coords = domain_coords['polygon']
                 polygon = Polygon([(zoom_coords['lon'][0], zoom_coords['lat'][0]),
                                 (zoom_coords['lon'][0], zoom_coords['lat'][1]),
                                 (zoom_coords['lon'][1], zoom_coords['lat'][1]),
